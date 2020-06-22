051ba638e47b5b66e5af9f92fa1d1b015b01c668
Steve <steve.skillcorn@leadiro.com>
2020-06-22 11:11:48 +0100

    Heath controller

---
+++ src/main/java/com/leadiro/starter/controller/HealthController.java
@@ -0,0 +1,26 @@
+package com.leadiro.starter.controller;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+    @Value("${app.version}") private String version;
+    @Value("${app.timestamp}") private String timestamp;
+
+    @GetMapping(value={ "/ping", "/health", "/starter/health" })
+    public Map<String, String> ping() {
+        Instant now = Instant.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG).withZone(ZoneId.of("UTC"));
+        return ImmutableMap.of("time", formatter.format(now), "version", version, "build", timestamp);
+    }
+
+}
